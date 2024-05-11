4e6e5aff1412e2cb966e79196c72c11d24962cd3
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-11 12:10:50 +0600

    11-5-2024

---
+++ 11-5-2024/A. Sum.cpp
@@ -0,0 +1,43 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-11 12:03:04
+    File: A. Sum.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(a.begin(), a.end())
+#define srtG(vll) sort(a.begin(), a.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        vi a(3);
+        fr(i, 0, 3) cin >> a[i];
+        srt(a);
+        if (a[0] + a[1] == a[2])
+            yes;
+        else
+            no;
+    }
+
+    return 0;
+}
