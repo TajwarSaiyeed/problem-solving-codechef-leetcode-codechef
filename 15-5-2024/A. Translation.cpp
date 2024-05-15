1036976ffa8f63e8f1f71df48f6cf7b2ac91485d
tajwarsaiyeed <rjabid36@gmail.com>
2024-05-15 22:34:01 +0600

    15-5-2024

---
+++ 15-5-2024/A. Translation.cpp
@@ -0,0 +1,40 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-05-15 22:28:00
+    File: A. Translation.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s, t;
+    cin >> s >> t;
+
+    reverse(s.begin(), s.end());
+
+    if (s == t)
+        yes;
+    else
+        no;
+
+    return 0;
+}
