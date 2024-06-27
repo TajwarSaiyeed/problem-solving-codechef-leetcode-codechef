4a0a44c484b4da7308804617f4215ba3c84dd03f
tajwarsaiyeed <rjabid36@gmail.com>
2024-06-27 23:31:35 +0600

    feat: Add C++ solutions for "Unique Number of Occurrences", "Vasya the Hipster", "Counting Words", and "Logical Sequence 2"

---
+++ 27-6-2024/Counting Words.cpp
@@ -0,0 +1,22 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-06-27 23:05:29
+    File: Counting Words.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    int n, m;
+    while (tc--)
+    {
+        cin >> n >> m;
+        cout << n * m << '\n';
+    }
+    return 0;
+}
+
+// Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
