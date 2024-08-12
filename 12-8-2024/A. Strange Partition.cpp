ba6ff9efbbde1568ba5da2ff14ef654b895eda4f
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-12 22:46:15 +0600

    feat: Add C++ solution for the "Strange Partition" problem

---
+++ 12-8-2024/A. Strange Partition.cpp
@@ -0,0 +1,59 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-12 10:50:52
+    File: A. Strange Partition.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srt(vll) sort(vll.begin(), vll.end())
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    ll n, x, mn_sum = 0, mx_sum = 0;
+    cin >> n >> x;
+    vll arr(n);
+    fr(i, 0, n)
+    {
+        cin >> arr[i];
+        mn_sum += arr[i];
+        mx_sum += (arr[i] + x - 1) / x;
+    }
+    cout << ((mn_sum + x - 1) / x) << " " << mx_sum << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/contest/1471/problem/A
