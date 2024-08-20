16354023ec3318ac1de4a6ecb6177b33f2157922
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-20 21:59:43 +0600

    feat: Add C++ solution for the "Valid Anagram" problem

---
+++ 20-8-2024/leetcode/242. Valid Anagram.cpp
@@ -0,0 +1,63 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-20 21:57:16
+    File: 242. Valid Anagram.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define fr(i, ivalue, n) for (int i = ivalue; i < n; i++)
+#define nfr(i, ivalue, n) for (int i = ivalue; i > n; i--)
+#define srtG(vll) sort(vll.begin(), vll.end(), greater<ll>())
+#define mps map<string, int>
+#define dt(n) fixed << setprecision(n)
+#define srt(s) sort(s.begin(), s.end())
+typedef long long ll;
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        srt(s), srt(t);
+        return s == t;
+    }
+};
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    Solution sol;
+    cout << sol.isAnagram(s, t) << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://leetcode.com/problems/valid-anagram/
