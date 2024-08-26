51acd95cddf368a6c13bdbe148a4196574d18427
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-26 23:45:31 +0600

    feat: Add C++ solution for the "Sugarcane Juice Business" problem

---
+++ 26-8-2024/Sugarcane Juice Business.cpp
@@ -0,0 +1,22 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-08-26 23:44:57
+    File: Sugarcane Juice Business.cpp
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x;
+    cin >> tc;
+    while (tc--)
+    {
+        cin >> x;
+        cout << x * 15 << '\n';
+    }
+    return 0;
+}
+
+// Problem : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE
