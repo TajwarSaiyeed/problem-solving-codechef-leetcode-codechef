2f3c18c3ea3cce611987346f3b249ff7eb905aa1
tajwarsaiyeed <rjabid36@gmail.com>
2024-11-20 07:08:51 +0600

    feat: Add C++ implementation for the "A. Indian Summer" problem with set-based solution for unique pairs

---
+++ 20-11-2024/A. Indian Summer.cpp
@@ -0,0 +1,54 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2024-11-20 07:03:58
+    File: A. Indian Summer.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    set<pair<string, string>> s;
+    string a, b;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        s.insert({a, b});
+    }
+    cout << s.size() << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    // cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem :
