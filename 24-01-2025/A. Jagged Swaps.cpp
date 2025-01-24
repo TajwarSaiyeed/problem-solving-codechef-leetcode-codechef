cfa07597e8d832bbd56f6e3463088d9a3a67b236
tajwarsaiyeed <rjabid36@gmail.com>
2025-01-24 20:41:56 +0600

    feat: Add C++ implementation for "A. Jagged Swaps" problem

---
+++ 24-01-2025/A. Jagged Swaps.cpp
@@ -0,0 +1,43 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2025-01-24 18:50:22
+    File: A. Jagged Swaps.cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl '\n'
+#define vi vector<int>
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    if (a[0] == 1)
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/problemset/problem/1896/A
