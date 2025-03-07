26d7c65886252e7b63317f70365aab560bb66df3
tajwarsaiyeed <rjabid36@gmail.com>
2025-03-07 19:18:57 +0600

    feat: Add C++ implementation for "A. Mahmoud and Ehab and the even-odd game" problem

---
+++ 07-03-2025/A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,50 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2025-03-07 19:14:17
+    File: A. Mahmoud and Ehab and the even-odd game.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << "Mahmoud" << nl;
+    else
+        cout << "Ehab" << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    // cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem :
