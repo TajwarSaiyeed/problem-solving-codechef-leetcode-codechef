2f0481ce75c6c65f484b0900c1c236ff6f048574
tajwarsaiyeed <rjabid36@gmail.com>
2025-04-10 21:19:07 +0600

    feat: Add C++ implementation for "A. Ideal Generator" problem

---
+++ 10-04-2025/A. Ideal Generator.cpp
@@ -0,0 +1,50 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2025-04-10 21:15:21
+    File: A. Ideal Generator.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int k;
+    cin >> k;
+    if (k % 2 == 1)
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem : https://codeforces.com/contest/2093/problem/A
