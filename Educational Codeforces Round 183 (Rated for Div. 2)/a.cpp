b9192308b0c0f92dec02383d9398290852754f7a
tajwarsaiyeed <rjabid36@gmail.com>
2025-10-06 21:12:47 +0600

    feat: Add solution for a problem in a.cpp

---
+++ Educational Codeforces Round 183 (Rated for Div. 2)/a.cpp
@@ -0,0 +1,51 @@
+/*
+    Author: Tajwar Saiyeed
+    Date: 2025-10-06 20:37:54
+    File: a.cpp
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define p1 cout << '1'
+#define p0 cout << '0'
+#define nl '\n'
+#define vll vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define dt(n) fixed << setprecision(n)
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int r = n % 3;
+    if (r == 0)
+        cout << 0 << nl;
+    else
+        cout << 3 - r << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Problem :
