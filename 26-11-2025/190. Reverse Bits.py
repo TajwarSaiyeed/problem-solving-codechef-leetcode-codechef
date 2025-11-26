429e7cff99aefcb4c9897f104ef77dbb8c1d0ae2
tajwarsaiyeed <rjabid36@gmail.com>
2025-11-26 18:31:46 +0600

    Add Python solution for 190. Reverse Bits

---
+++ 26-11-2025/190. Reverse Bits.py
@@ -0,0 +1,24 @@
+class Solution:
+    def int_to_bits(self, n: int) -> str:
+        if n < 0:
+            n = (1 << 32) + n
+
+        bits = []
+
+        for i in range(32):
+            bit = n & 1
+            bits.append(str(bit))
+
+            n >>=1
+        
+        bits.reverse()
+        return "".join(bits)
+
+
+    def reverseBits(self, n: int) -> int:
+        # x = format(n & 0xFFFFFFFF, '032b')
+        x = self.int_to_bits(n)
+        x = x[::-1]
+        return int(x, 2)
+
+# https://leetcode.com/problems/reverse-bits/
