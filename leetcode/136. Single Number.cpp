b03d33cf8da4b5e243a1db59fe88e5ea67bde69f
tajwarsaiyeed <rjabid36@gmail.com>
2025-12-31 19:19:08 +0600

    feat: Implement solution for Single Number problem

---
+++ leetcode/136. Single Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = nums[0];
+        for (int x = 1; x < nums.size(); ++x)
+            ans ^= nums[x];
+        return ans;
+    }
+};
