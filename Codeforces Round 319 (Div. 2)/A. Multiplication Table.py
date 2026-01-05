2354f950b1bfab0344f2623e36c90a6bd8a27c97
tajwarsaiyeed <rjabid36@gmail.com>
2026-01-05 07:19:55 +0600

    feat: Add solution for A. Multiplication Table problem

---
+++ Codeforces Round 319 (Div. 2)/A. Multiplication Table.py
@@ -0,0 +1,16 @@
+def main():
+    n, x = map(int, input().split())
+
+    cnt = 0
+
+    for i in range(1, min(n, x) + 1):
+        if x % i == 0:
+            j = x // i
+            if j <= n:
+                cnt += 1
+
+    print(cnt)
+
+
+if __name__ == "__main__":
+    main()
