297801b336d6716b87beb4fb3bbedbee3c3febc9
tajwarsaiyeed <rjabid36@gmail.com>
2026-01-31 09:36:43 +0600

    feat: Add solution for Codeforces problem 1877. Goals of Victory

---
+++ Codeforces Round 902 (Div. 2, based on COMPFEST 15 - Final Round)/A. Goals of Victory.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input())
+    arr = sum(list(map(int, input().split())))
+    print(-arr)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        main()
+
+# https://codeforces.com/problemset/problem/1877/A
