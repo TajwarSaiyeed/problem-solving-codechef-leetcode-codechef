97fc726d1adcddeeb24ab78288223c6113d2d2cb
tajwarsaiyeed <rjabid36@gmail.com>
2026-03-19 23:10:08 +0600

    refactor: Rename parameter and improve code readability in solution for LeetCode problem 3212

--- leetcode/3212. Count Submatrices With Equal Frequency of X and Y.py
+++ leetcode/3212. Count Submatrices With Equal Frequency of X and Y.py
@@ -1,6 +1,10 @@
+from itertools import accumulate
+from typing import List
+
+
 class Solution:
-    def numberOfSubmatrices(self, g: List[List[str]]) -> int:
-        p = (accumulate(map({"X": 1, "Y": 1j, ".": 0}.get, r)) for r in g)
+    def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
+        p = (accumulate(map({"X": 1, "Y": 1j, ".": 0}.get, r)) for r in grid)
         return sum(0 < q.real == q.imag for c in zip(*p) for q in accumulate(c))
 
 
